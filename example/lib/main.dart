23d47e9ec3c2df9ba593d7f84a2a8149070e7aa3
Simon Lightfoot <simon@devangels.london>
2019-11-04 23:44:08 +0000

    Update example code to use tabs to test save and restore offset.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -25,42 +25,64 @@ class _ExampleScreenState extends State<ExampleScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Example'),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.arrow_downward),
-            onPressed: () {
-              _infiniteController.animateTo(_infiniteController.offset + 2000.0, duration: const Duration(milliseconds: 250), curve: Curves.easeIn);
-            },
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Example'),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.arrow_downward),
+              onPressed: () {
+                _infiniteController.animateTo(_infiniteController.offset + 2000.0,
+                    duration: const Duration(milliseconds: 250), curve: Curves.easeIn);
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.arrow_upward),
+              onPressed: () {
+                _infiniteController.animateTo(_infiniteController.offset - 2000.0,
+                    duration: const Duration(milliseconds: 250), curve: Curves.easeIn);
+              },
+            ),
+          ],
+          bottom: const TabBar(
+            tabs: <Widget>[
+              Tab(text: 'First'),
+              Tab(text: 'Second'),
+              Tab(text: 'Third'),
+            ],
           ),
-          IconButton(
-            icon: const Icon(Icons.arrow_upward),
-            onPressed: () {
-              _infiniteController.animateTo(_infiniteController.offset - 2000.0, duration: const Duration(milliseconds: 250), curve: Curves.easeIn);
-            },
-          ),
-        ],
-      ),
-      body: InfiniteListView.separated(
-        controller: _infiniteController,
-        itemBuilder: _buildItem,
-        separatorBuilder: (BuildContext context, int index) => const Divider(height: 2.0),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            _buildTab(0),
+            _buildTab(1),
+            _buildTab(2),
+          ],
+        ),
       ),
     );
   }
 
-  Widget _buildItem(BuildContext context, int index) {
-    return Material(
-      child: InkWell(
-        onTap: () {},
-        child: ListTile(
-          title: Text('Item #$index'),
-          subtitle: Text('Subtitle $index'),
-          trailing: const Icon(Icons.chevron_right),
-        ),
-      ),
+  Widget _buildTab(int tab) {
+    return InfiniteListView.separated(
+      key: PageStorageKey(tab),
+      controller: _infiniteController,
+      itemBuilder: (BuildContext context, int index) {
+        return Material(
+          child: InkWell(
+            onTap: () {},
+            child: ListTile(
+              title: Text('Tab $tab Item #$index'),
+              subtitle: Text('Subtitle $index'),
+              trailing: const Icon(Icons.chevron_right),
+            ),
+          ),
+        );
+      },
+      separatorBuilder: (BuildContext context, int index) => const Divider(height: 2.0),
+      anchor: 0.5,
     );
   }
 }
